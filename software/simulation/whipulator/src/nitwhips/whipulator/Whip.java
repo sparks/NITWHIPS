a4d89a411c619f24638a7765838d107626a6ae44
Severin Smith <info@smallbutdigital.com>
2011-07-31 18:16:02 -0400

    Working on the simulation of vibration interface

--- software/simulation/whipulator/src/nitwhips/whipulator/Whip.java
+++ software/simulation/whipulator/src/nitwhips/whipulator/Whip.java
@@ -2,49 +2,72 @@ package nitwhips.whipulator;
 
 public class Whip {
 	
+	/* Static constants */
+	
 	static int MAX_MODES = 5;
 	static int HEIGHT = 50;
 	static int RADIUS = 1;
+
+	/* Drawing */
+		
+	Whipulator p;
+	boolean selected;
+	
+	/* Whip information */
 	
 	int id;
 	float base_x, base_y;
 	
-	boolean active;
-	
+	/* Sensor data */
+		
 	float tip_x, tip_y;
-	float[][] modes;
 	
-	int[][][] leds;
+	float[][] tip_history;
+	float tip_max[];
+	int tip_history_index;
 	
+	float[][][] modes;
 	//Modes are roughly the fourier tansform of the x and y accelerometer signal. 
 	//The 0th mode is the DC offset or the lean of the pole.
 	//In reality this will be a little trickier to compute
 	
 	SimEngine engine;
 	
-	Whipulator p;
-
+	/* LED Output */
+	
+	int[][][] leds;
+	
 	public Whip(Whipulator p, int id, float base_x, float base_y) {
+		/* Drawing */
+	
 		this.p = p;
-
+		selected = false;
+		
+		/* Whip information */
+	
 		this.id = id;
-
 		this.base_x = base_x;
 		this.base_y = base_y;
-		
-		active = false;
+	
+		/* Sensor data */
 		
 		tip_x = 0;
 		tip_y = 0;
-		
-		modes = new float[2][MAX_MODES];
-		
-		leds = new int[5][4][3]; //[y][theta][color]
+
+		tip_history = new float[0][2];
+		tip_max = new float[2];
+		tip_history_index = 0;
+
+		modes = new float[MAX_MODES][2][2]; //[mode][x/y][real/complexe] (where real is cos and complexe is sin)
 		
 		engine = new SimEngine();
+		
+		/* LED Output */
+
+		leds = new int[5][4][3]; //[panel][face][RGB]
 	}
 	
-	public void draw() {
+	public void drawModel() {
 		p.pushMatrix();
 		p.translate(base_x, 0, base_y);
 		
@@ -55,99 +78,218 @@ public class Whip {
 		p.popMatrix();
 	}
 	
-	void drawPanel(int panel, int face_colors[][]) {
-		p.stroke(255);
+	public void drawTipWave() {
+		if(isSelected()) {
+			int x = p.width-350;
+			int y = p.height-250;
+			int width = 350;
+			int height = 250;
+		
+			if(tip_history.length != width) {
+				tip_history = new float[width][2];
+				tip_max = new float[2];
+				tip_history_index = 0;
+			}
 
-		int h_sub = HEIGHT/leds.length;
+			p.strokeWeight(3);
+			p.stroke(100);
+			p.fill(0);
+			p.rect(x, y, width-1, height/2-1);
+			p.rect(x, y+height/2, width-1, height/2-1);
+		
+			p.strokeWeight(3);
+			p.stroke(100);
+		
+			for(int i = 0;i < tip_history.length;i++) {
+				p.point(i+x, y+p.map(tip_history[(tip_history_index+i)%tip_history.length][0], -tip_max[0], tip_max[0], 5, height/2-5));
+				p.point(i+x, y+height/2+p.map(tip_history[(tip_history_index+i)%tip_history.length][1], -tip_max[1], tip_max[1], 5, height/2-5));
+			}
+		}
+	}
+	
+	public void drawModeControls() {
+		if(isSelected()) {
+			int x = 0;
+			int y = p.height-250;
+			int width = p.width-350;
+			int height = 250;
+			int padding = 3;
+		
+			p.strokeWeight(3);
+			p.stroke(100);
+			p.fill(0);
+			p.rect(x, y, width-1, height-1);
+	
+			p.strokeWeight(1);
+			
+			p.stroke(255);
+			p.fill(255);
+			
+			float band_width = width/MAX_MODES/4;
+			
+			
+			for(int i = 0;i < MAX_MODES;i++) {
+				for(int j = 0;j < 2;j++) {
+					for(int k = 0;k < 2;k++) {
+						p.rect(padding+(i*4+j*2+k)*band_width, y+height/2, band_width-2*padding, -p.map(modes[i][j][k], -1, 1, -(height-2*padding)/2, (height-2*padding)/2));
+					}
+				}
+			}
+		}
+	}
+	
+	void drawPanel(int panel, int face_colors[][]) {
+		if(!selected) p.stroke(100);
+		else p.stroke(255);
+		p.strokeWeight(3);
+
+		int panel_height = HEIGHT/leds.length;
 
 		p.beginShape(p.QUADS);
 
 		p.fill(face_colors[0][0], face_colors[0][1], face_colors[0][2]); 
 
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2+RADIUS);
 
 		p.fill(face_colors[1][0], face_colors[1][1], face_colors[1][2]); 
 
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2+RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2+RADIUS);
 
 		p.fill(face_colors[2][0], face_colors[2][1], face_colors[2][2]); 
 
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2-RADIUS);
 
 		p.fill(face_colors[3][0], face_colors[3][1], face_colors[3][2]); 
 
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2-RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2-RADIUS);
 
 		p.fill(100);        
 
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2-RADIUS, -(panel+1)*h_sub, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -(panel+1)*panel_height, base_y/2+RADIUS);
 
 		p.fill(100);        
 
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2-RADIUS);
-		p.vertex(base_x/2+RADIUS, -panel*h_sub, base_y/2+RADIUS);
-		p.vertex(base_x/2-RADIUS, -panel*h_sub, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2-RADIUS);
+		p.vertex(base_x/2+RADIUS, -panel*panel_height, base_y/2+RADIUS);
+		p.vertex(base_x/2-RADIUS, -panel*panel_height, base_y/2+RADIUS);
 
 		p.endShape();
+		
+		p.strokeWeight(1);
 	}
 	
-	public void setActive(boolean active) {
-		this.active = active;
+	public boolean isSelected() {
+		return selected;
 	}
 	
-	public boolean isActive() {
-		return active;
+	public void selected(boolean selected) {
+		this.selected = selected;
 	}
-
-	public void setMode(int xy, int mode, float value) {
-		if(mode >= 0 && mode < MAX_MODES && xy >= 0 && xy < 2) {
-			modes[xy][mode] = value;
+	
+	public void setMode(int mode, float real_value_x, float img_value_x, float real_value_y, float img_value_y) {
+		if(mode >= 0 && mode < MAX_MODES) {
+			modes[mode][0][0] = real_value_x;
+			modes[mode][0][1] = img_value_x;
+			modes[mode][1][0] = real_value_y;
+			modes[mode][1][1] = img_value_y;
 		} else {
 			System.err.println("Invalid mode data given");
 		}
 	}
 	
-	public void setModes(float[][] modes) {
-		if(modes.length >= 2 && modes[0].length >= MAX_MODES) this.modes = modes;
+	public void setModes(float[][][] modes) {
+		if(modes.length >= MAX_MODES && modes[0].length >= 2 && modes[0][0].length >= 2) this.modes = modes;
+	}
+	
+	public float[][] getMode(int mode) {
+		return modes[mode];
+	}
+
+	public float getMode(int mode, int x_y, int real_img) {
+		return modes[mode][x_y][real_img];
+	}
+	
+	public void mousePressed() {
+		if(isSelected()) {
+			mouseAction();
+		}
+	}
+	
+	public void mouseDragged() {
+		if(isSelected()) {
+			mouseAction();
+		}
+	}
+
+	public void mouseAction() {
 	}
 
 	class SimEngine implements Runnable {
-		boolean running = true;
 		
+		Thread sim_thread;
+		boolean running = true;
+		int start_time;
+
 		public SimEngine() {
 			running = true;
-			Thread t = new Thread(this);
-			t.setDaemon(true);
-			t.start();
+		
+			start_time = p.millis();
+		
+			sim_thread = new Thread(this);
+			sim_thread.setDaemon(true);
+			sim_thread.start();
 		}
 
 		public void run() {
 			while(running) {
+				int time = p.millis()-start_time;
 				tip_x = 0;
 				tip_y = 0;
+				
+				for(int i = 0;i < MAX_MODES;i++) {
+					tip_x += modes[i][0][0]*p.cos(6*p.PI*i*time/1000)+modes[i][0][1]*p.sin(6*p.PI*i*time/1000);
+					tip_y += modes[i][1][0]*p.cos(6*p.PI*i*time/1000)+modes[i][1][1]*p.sin(6*p.PI*i*time/1000);
+				}
+				
+				if(tip_history.length != 0) {
+					tip_history[tip_history_index%tip_history.length][0] = tip_x;
+					tip_history[tip_history_index%tip_history.length][1] = tip_y;
+
+					if(tip_x > tip_max[0]) tip_max[0] = tip_x;
+					if(tip_y > tip_max[1]) tip_max[1] = tip_y;
+					
+					tip_history_index++;
+				}
+				
 				try {
-					Thread.sleep(10);
+					Thread.sleep(5);
 				} catch(Exception e) {
 					
 				}
 			}
 		}
+		
+		public void stop() {
+			running = false;
+			while(sim_thread.isAlive()) Thread.yield();
+		}
+		
 	}
 
 }
--- software/simulation/whipulator/src/nitwhips/whipulator/Whipulator.java
+++ software/simulation/whipulator/src/nitwhips/whipulator/Whipulator.java
@@ -15,19 +15,26 @@ public class Whipulator extends PApplet {
 	float rot_x, rot_y;
 	
 	PFont font;
-		
+	
+	Button[] view_buttons;
+	Button[] whip_buttons;
+	
 	public void setup() {
-		size(1000, 800, OPENGL);
+		size(1000, 900, OPENGL);
 		background(0);
-		
-		setMode(WhipulatorMode.PERSPECTIVE);
-		
+				
 		whips = new Whip[] {
 			new Whip(this, 0, 0, 0),
 			new Whip(this, 0, 10, 0),
 			new Whip(this, 0, -20, 5),
 			new Whip(this, 0, 15, 10)
 		};
+		
+		whips[0].setMode(0, 0, 0, 0, 0);
+		whips[0].setMode(1, 0, 0, 0, 0);
+		whips[0].setMode(2, 0, 0, 0, 0);
+		whips[0].setMode(3, 0, 0, 0, 0);
+		whips[0].setMode(4, 0, 0, 0, 0);
 		
 		grid_size = 150;
 		grid_div = 15;
@@ -37,6 +44,43 @@ public class Whipulator extends PApplet {
 		
 		font = createFont("Helvetica", 14);
 		textFont(font, 12);
+		
+		view_buttons = new Button[4];
+		for(int i = 0;i < 4;i++) {
+			final int tmp = i;
+			view_buttons[i] = new Button(this, 10+30*i, 10, new EventListener() {
+				public void event(int arg) {
+					for(int j = 0;j < view_buttons.length;j++) view_buttons[j].clear();
+					setMode(WhipulatorMode.values()[tmp]);
+				}
+			});
+		}
+		
+		view_buttons[3].select();
+		
+		whip_buttons = new Button[whips.length+1];
+
+		for(int i = 0;i < whips.length;i++) {
+			final int tmp = i;
+			whip_buttons[i] = new Button(this, 10, 40+30*(i), new EventListener() {
+				public void event(int arg) {
+					for(int j = 0;j < whip_buttons.length;j++) whip_buttons[j].clear();
+					for(int j = 0;j < whips.length;j++) whips[j].selected(false);
+					whips[tmp].selected(true);
+				}
+			});
+		}
+		
+		whip_buttons[whip_buttons.length-1] = new Button(this, 10, 40*(whip_buttons.length-1), new EventListener() {
+			public void event(int arg) {
+				for(int j = 0;j < whip_buttons.length;j++) whip_buttons[j].clear();
+				for(int j = 0;j < whips.length;j++) whips[j].selected(false);
+			}
+		});
+		
+		whip_buttons[whip_buttons.length-1].select();
+
+		setMode(WhipulatorMode.PERSPECTIVE);
 	}
 	
 	public void draw() {
@@ -54,11 +98,16 @@ public class Whipulator extends PApplet {
 		}
 	
 		for(Whip whip : whips) {
-			whip.draw();
+			whip.drawModel();
 		}
 		
 		popMatrix();
-		
+
+		for(Whip whip : whips) {
+			whip.drawTipWave();
+			whip.drawModeControls();
+		}
+
 		//Text
 		
 		fill(255);
@@ -66,39 +115,37 @@ public class Whipulator extends PApplet {
 		text(mode.toString(), width-150, 20);
 		
 		//Buttons
-						
-		strokeWeight(3);
-		stroke(150);
-		for(int i = 0;i < 4;i++) {
-			if(mode.getNum() == i) fill(255);
-			else if(mouseX > 10+20*i && mouseX < 10+20*(i+1) && mouseY > 10 && mouseY < 35) fill(100);
-			else fill(0);
-			rect(10+20*i, 10, 15, 15);
-		}
 		
-		strokeWeight(1);
+		for(Button b : view_buttons) b.draw();
+		for(Button b : whip_buttons) b.draw();		
 	}
 	
 	public void mousePressed() {
 		prev_mouse_x = mouseX;
 		prev_mouse_y = mouseY;
+		
+		for(Whip whip : whips) whip.mousePressed();
 	}
 	
 	public void mouseDragged() {
-		rot_x -= (float)(mouseY-prev_mouse_y)/height*PI;
-		rot_y += (float)(mouseX-prev_mouse_x)/width*PI;
+		if(whip_buttons[whip_buttons.length-1].isSelected() || mouseY < height-250) {
+			rot_x -= (float)(mouseY-prev_mouse_y)/height*PI;
+			rot_y += (float)(mouseX-prev_mouse_x)/width*PI;
 		
-		prev_mouse_x = mouseX;
-		prev_mouse_y = mouseY;
+			prev_mouse_x = mouseX;
+			prev_mouse_y = mouseY;
 		
-		mode = WhipulatorMode.PERSPECTIVE;
+			mode = WhipulatorMode.PERSPECTIVE;
+			for(Button b : view_buttons) b.clear();
+			view_buttons[mode.getNum()].select();
+		}
+
+		for(Whip whip : whips) whip.mouseDragged();
 	}
 		
 	public void mouseReleased() {
-		for(int i = 0;i < 4;i++) {
-			if(mouseX > 10+20*i && mouseX < 10+20*(i+1) && mouseY > 10 && mouseY < 35) 
-			for(WhipulatorMode w : WhipulatorMode.values()) if(w.getNum() == i) setMode(w);
-		}
+		for(Button b : view_buttons) b.mouseReleased();
+		for(Button b : whip_buttons) b.mouseReleased();
 	}
 	
 	public void keyPressed() {
@@ -132,6 +179,9 @@ public class Whipulator extends PApplet {
 				rot_y = PI/2;
 				break;
 		}
+		
+		for(Button b : view_buttons) b.clear();
+		view_buttons[mode.getNum()].select();
 		
 		this.mode = mode;
 	}
