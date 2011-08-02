e494857372fb8159a8b11be9e4afa8bd58d9d4a6
2000dB <2000dB@gmail.com>
2011-08-01 21:22:00 -0400

    bleh

--- software/firmware/main.cpp
+++ software/firmware/main.cpp
@@ -1,6 +1,4 @@
-#include "timer.h"
 #include "controller.h"
-
 #include "wirish.h"
 
 /*
@@ -11,34 +9,110 @@
  */
 
 
-void init_rs485(void);
-void parse_message(uint8 []);
-void transmit_buffer(uint8 [], uint8);
+class LEDStrip {
+private:
+  uint8 red_pin, green_pin, blue_pin;
+  uint8 pixels[4];
+  uint8 pixel_status;
+  uint8 status;
+public:
+  uint16 r, g, b;
+  void init_strip(uint8, uint8, uint8, uint8, uint8, uint8, uint8);
+  void set_color(uint16, uint16, uint16);
+  void blackout(void);
+  void set_pixels(uint8);
+} strip;
 
-// test
-void init_effect(void);
-void start_effect(uint8);
-void update_effect(void);
+void LEDStrip::init_strip(uint8 rp, uint8 gp, uint8 bp, uint8 p1, uint8 p2, uint8 p3, uint8 p4) {
+  red_pin = rp;
+  green_pin = gp;
+  blue_pin = bp;
 
-uint16 r = 0;
-uint16 g = 0;
-uint16 b = 0;
+  pinMode(red_pin, PWM);
+  pinMode(green_pin, PWM);
+  pinMode(blue_pin, PWM);
 
-uint16 r_step = 0;
-uint16 g_step = 0;
-uint16 b_step = 0;
+  pixels[0] = p1;
+  pixels[1] = p2;
+  pixels[2] = p3;
+  pixels[3] = p4;
+  pinMode(pixels[0], OUTPUT);
+  pinMode(pixels[1], OUTPUT);
+  pinMode(pixels[2], OUTPUT);
+  pinMode(pixels[3], OUTPUT);
 
-uint16 r_target = 0xFFFF;
-uint16 g_target = 0x0000;
-uint16 b_target = 0xFFFF;
+  blackout();
+}
 
+void LEDStrip::set_pixels(uint8 mapping) {
+  pixel_status = mapping;
+  uint8 j = 0;
+  for(uint8 i = 0x01;i < 0x10;i <<= 1) {
+    digitalWrite(j++, mapping & i);
+  }
+}
+
+void LEDStrip::set_color(uint16 r, uint16 g, uint16 b) {
+  r = r;
+  g = g;
+  b = b;
+  pwmWrite(red_pin, r);
+  pwmWrite(green_pin, g);
+  pwmWrite(blue_pin, b);
+}
+
+void LEDStrip::blackout(void) {
+  set_color(0, 0, 0);
+}
+
+LEDStrip strip_1;
+
+class Effect {
+  #define RUNNING 0
+  #define DONE 1
+private:
+  uint16 target_red, target_green, target_blue;
+  LEDStrip strp;
+public:
+  uint16 time, tick;
+  uint8 status;
+  void start_effect(LEDStrip, uint16, uint16, uint16, uint16);
+  void effect_done(void);
+  void update(void);
+} effect;
+
+void Effect::start_effect(LEDStrip strp, uint16 rt, uint16 gt, uint16 bt, uint16 tt) {
+  target_red = rt;
+  target_green = gt;
+  target_blue = bt;
+  time = tt;
+  strp = strp;
+  status = RUNNING;
+}
+
+void Effect::effect_done(void) {
+  status = DONE;
+}
+
+void Effect::update(void) {
+  //strp.r += int((target_red - strp.r) / tick);
+  strp.set_color(0xFFFF, 0, 0);
+    //tick++;
+}
+
+Effect efx;
+  
 #define BUFFER_SIZE 1
 uint8 rx_buffer_index = 0;
 uint8 rx_buffer[BUFFER_SIZE];
 
+void init_rs485(void);
+void parse_message(uint8 []);
+void transmit_buffer(uint8 [], uint8);
+
 void setup() {
-  init_effect();
   init_rs485();
+  strip_1.init_strip(RED_1, GREEN_1, BLUE_1, PIXEL_1_1, PIXEL_1_2, PIXEL_1_3, PIXEL_1_4); // LOL
 }
 
 void loop() {
@@ -46,8 +120,8 @@ void loop() {
 
   if (RS485.available()) {
     uint8 incoming = RS485.read();
-    rx_buffer[rx_buffer_index++] = incoming;
 
+    rx_buffer[rx_buffer_index++] = incoming;
     if (rx_buffer_index == BUFFER_SIZE) {
       // Buffer full
       rx_buffer_index = 0; // Reset buffer index
@@ -55,31 +129,14 @@ void loop() {
     }
   }
 
-  update_effect();
+  efx.update();
+    
 }
 
-void update_effect(void) {
-  r += r_step;
-  g += g_step;
-  b += b_step;
 
-  pwmWrite(RED_1, r);
-  pwmWrite(GREEN_1, g);
-  pwmWrite(BLUE_1, b);
-  
-}
-
-void start_effect(uint8 effect_id) {
-  r_step = uint16((r_target - r) / 1000);
-  g_step = uint16((g_target - g) / 1000);
-  b_step = uint16((b_target - b) / 1000);
-}
-  
-  
 
 void parse_message(uint8 buffer[]) {
-  uint8 effect_id = (0x03 & *buffer);
-  start_effect(effect_id);
+  efx.start_effect(strip_1, 0xFFFF, 0, 0, 1000);
 }
 
 void transmit_buffer(uint8 buffer[], uint8 buffer_length) {
@@ -92,16 +149,6 @@ void transmit_buffer(uint8 buffer[], uint8 buffer_length) {
     digitalWrite(RS485_DIR_PIN, LOW);
   }
 }
-
-void init_effect(void) {
-  pinMode(RED_1, PWM);
-  pwmWrite(RED_1, 0);
-  pinMode(GREEN_1, PWM);
-  pwmWrite(GREEN_1, 0);
-  pinMode(BLUE_1, PWM);
-  pwmWrite(BLUE_1, 0);
-}
-
 
 void init_rs485(void) {
   RS485.begin(BAUD_RATE);
