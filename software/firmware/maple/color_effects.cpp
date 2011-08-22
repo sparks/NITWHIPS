62beae09c8f0296b6eb4cfdf4678722b7c1a82fb
Vincent de Belleval <2000db@gmail.com>
2011-08-22 03:50:46 -0400

    Cleanup

--- software/firmware/maple/color_effects.cpp
+++ software/firmware/maple/color_effects.cpp
@@ -2,21 +2,43 @@
 #include "controller.h"
 #include "wirish.h"
 
+/* Color Effect */
+
+ColorEffect::ColorEffect(uint16 p) {
+  period = p;
+  status = RUNNING;
+};
+
 uint16 ColorEffect::blend(const uint16 o, const uint16 m) {
   switch(blend_mode) {
-  case BLEND_AND:
-    return o & m;
-  case BLEND_OR:
-    return o | m;
-  case BLEND_XOR:
-    return o ^ m;
-  default: return 0xFFFF;
+    case BLEND_AND:
+      return o & m;
+    case BLEND_OR:
+      return o | m;
+    case BLEND_XOR:
+      return o ^ m;
+    default: return 0xFFFF;
   }
 }
+
+/* Color Static */
+
+CStatic::CStatic(uint16 p) : ColorEffect(p) {
+   period = p;
+   blend_mode = BLEND_OR;
+};
 
 uint16 CStatic::update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index) {
   return blend(channel, target_colors[side][channel_index]);
 }
+
+/* Color Linear Fade */
+
+CLinearFade::CLinearFade(uint16 p) : ColorEffect(p) {
+  period = p;
+  status = RUNNING;
+  blend_mode = BLEND_AND;
+};
 
 uint16 CLinearFade::update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index) {
   uint16 val;
@@ -24,16 +46,28 @@ uint16 CLinearFade::update(uint16 tick, uint16 side, uint16 channel, uint8 chann
   return blend(channel, val);
 }
 
+/* Color Circle */
+
+CCircle::CCircle(uint16 p): ColorEffect(p) {
+  period = p;
+  length = NUM_SIDES;
+  direction = DIR_UP;
+  offset = 0;
+  if(direction == DIR_UP) position = offset;
+  else position = length-1;
+};
+
 uint16 CCircle::update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index) {
-    uint16 val;
-    if(position == side) val = blend(channel, 0xFFFF);
-    else val = blend(channel, 0x0000);
-    if(tick % period == 0 && side == 0 && channel_index == 0) {
-      if(direction == DIR_UP) position = ++position % length;
-      else {
-	if(position == 0) position = length;
-	position = --position % length;
-      }
+  uint16 val;
+  if(position == side) val = blend(channel, 0xFFFF);
+  else val = blend(channel, 0x0000);
+  
+  if(tick % period == 0 && side == 0 && channel_index == 0) {
+    if(direction == DIR_UP) position = ++position % length;
+    else {
+      if(position == 0) position = length;
+      position = --position % length;
     }
-    return val;
   }
+  return val;
+}
--- software/firmware/maple/color_effects.h
+++ software/firmware/maple/color_effects.h
@@ -5,54 +5,38 @@
 #define _COLOR_EFFECTS_H_
 
 class ColorEffect {
- public:
-  uint16 * base_colors;
-  uint16 target_colors[NUM_SIDES][NUM_RGB]; // 3 values for target RGB
-  uint8 status;
-  uint16 period; // Time variable
-  uint8 position;
-  uint8 offset;
-  uint8 length;
-  uint8 direction;
-  uint8 blend_mode;
-  ColorEffect(uint16 p) {
-    period = p;
-    status = RUNNING;
-  };
-  uint16 blend(const uint16 o, const uint16 m);
-  virtual uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index) =0;
+  public:
+    uint16* base_colors;
+    uint16 target_colors[NUM_SIDES][NUM_RGB]; // 3 values for target RGB
+    uint8 status;
+    uint16 period; // Time variable
+    uint8 position;
+    uint8 offset;
+    uint8 length;
+    uint8 direction;
+    uint8 blend_mode;
+    
+    ColorEffect(uint16 p);
+    uint16 blend(const uint16 o, const uint16 m);
+    virtual uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index) =0;
 };
 
 class CStatic: public ColorEffect {
- public:
- CStatic(uint16 p): ColorEffect(p) {
-    period = p;
-    blend_mode = BLEND_OR;
-  };
-  uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
+  public:
+    CStatic(uint16 p);
+    uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
 };
 
 class CLinearFade: public ColorEffect {
- public:
- CLinearFade(uint16 p): ColorEffect(p) {
-    period = p;
-    status = RUNNING;
-    blend_mode = BLEND_AND;
-  };
-  uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
+  public:
+    CLinearFade(uint16 p);
+    uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
 };
 
 class CCircle: public ColorEffect {
- public:
- CCircle(uint16 p): ColorEffect(p) {
-    period = p;
-    length = NUM_SIDES;
-    direction = DIR_UP;
-    offset = 0;
-    if(direction == DIR_UP) position = offset;
-    else position = length-1;
-  };
-  uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
+  public:
+    CCircle(uint16 p);
+    uint16 update(uint16 tick, uint16 side, uint16 channel, uint8 channel_index);
 };
 
 #endif
